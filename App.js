24262a477ad8e8dc8eb48e2081cc3b826b309f55
Gabriel Rocha <gabrielras12@hotmail.com>
2023-04-25 08:12:59 -0300

    Refatorar Código

--- App.js
+++ App.js
@@ -5,18 +5,18 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.column}>
-        <View style={styles.block_one} />
-        <View style={styles.block_two} />
-        <View style={styles.block_three} />
+        <View style={[styles.block, { backgroundColor: '#9d40e9', aspectRatio: 0.9 }]} />
+        <View style={[styles.block, { backgroundColor: '#61a9fe', aspectRatio: 0.9 }]} />
+        <View style={[styles.block, { backgroundColor: '#7638ff', aspectRatio: 0.5 }]} />
       </View>
       <View style={styles.column}>
-        <View style={styles.block_four} />
-        <View style={styles.block_five}>
-          <View style={styles.block_five_text_wrapper}>
+        <View style={[styles.block, { backgroundColor: '#9fbeff', aspectRatio: 0.9 }]} />
+        <View style={[styles.block, { backgroundColor: '#61a9fe', aspectRatio: 0.36 }]}>
+          <View style={[styles.block_five_text_wrapper, { transform: [{ rotate: '-90deg' }] }]}>
             <Text style={styles.block_five_text}>Flexbox</Text>
           </View>
         </View>
-        <View style={styles.block_six} />
+        <View style={[styles.block, { backgroundColor: '#7800c8', aspectRatio: 3 }]} />
       </View>
     </View>
   );
@@ -34,38 +34,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingHorizontal: 3
   },
-  block_one: {
+  block: {
     width: '100%',
-    aspectRatio: 0.9,
-    backgroundColor: '#9d40e9',
-    marginVertical: 1,
-    padding: 2,
-  },
-  block_two: {
-    width: '100%',
-    aspectRatio: 0.9,
-    backgroundColor: '#61a9fe',
-    marginVertical: 1,
-    padding: 2,
-  },
-  block_three: {
-    width: '100%',
-    aspectRatio: 0.5,
-    backgroundColor: '#7638ff',
-    marginVertical: 1,
-    padding: 2,
-  },
-  block_four: {
-    width: '100%',
-    aspectRatio: 0.9,
-    backgroundColor: '#9fbeff',
-    marginVertical: 1,
-    padding: 2,
-  },
-  block_five: {
-    width: '100%',
-    aspectRatio: 0.36,
-    backgroundColor: '#61a9fe',
     marginVertical: 1,
     padding: 2,
   },
@@ -77,14 +47,6 @@ const styles = StyleSheet.create({
   block_five_text: {
     fontWeight: 'bold',
     color: '#fff',
-    transform: [{ rotate: '-90deg' }],
     fontSize: 55,
   },
-  block_six: {
-    width: '100%',
-    aspectRatio: 3,
-    backgroundColor: '#7800c8',
-    marginVertical: 1,
-    padding: 2,
-  }
 });
